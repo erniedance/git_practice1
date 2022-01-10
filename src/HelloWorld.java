18155fc2c108570eb7ea7a8469111e8b2bcfa19f
ernie <edance1775@gmail.com>
2022-01-10 14:44:46 -0500

    more work 1 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
 
-    // this is our second text and will commit soon "work is done here"
+     // this is our second text and will commit soon "work is done here"
 
     // this is our third text and will commit soon "                   "
+
+    // Doing awesome work now!
+    // awesome work is done and ready to be committed
+    // and ready to push to remote repository
 }
