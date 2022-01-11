d60dce60d2d3b0dc15278d8503243cec1be26fa8
ernie <edance1775@gmail.com>
2022-01-11 10:59:46 -0500

    work 1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,16 @@
 public class HelloWorld {
 
-     // this is our second text and will commit soon "work is done here"
+    // this is our second text and will commit soon "work is done here"
 
     // this is our third text and will commit soon "                   "
 
     // Doing awesome work now!
     // awesome work is done and ready to be committed
     // and ready to push to remote repository
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
 
 }
